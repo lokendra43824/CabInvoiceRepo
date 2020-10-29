420eab3449caf3259388c6e09ff948458229eadf
Lokendra <lokilokendra12345@gmail.com>
2020-10-29 23:10:46 +0530

    [lokendra] first commit

---
+++ NUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+using CabInvoice;
+using NUnit.Framework;
+
+namespace NUnitTestProject1
+{
+    public class Tests
+    {
+        InvoiceGenerator invoiceGenerator = null;
+        [SetUp]
+        public void GivenDistanceAndTimeShouldReturnTotalFare()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double distance = 2.0;
+            int time = 5;
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double expected = 25;
+            Assert.AreEqual(expected, fare);
+        }
+
+
+    }
+}
